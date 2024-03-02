17b675ec21aa205d0dcebbf09be3fce0886b20e3
Julian Na <julian88naranjo@gmail.com>
2024-03-02 12:30:15 -0500

    hh

---
+++ poo.js
@@ -0,0 +1,31 @@
+
+let almacen = [
+    {
+        marca:'iphone',
+        precio:1500000,
+        garantia:true,
+        colores:{
+            o1:'Blanco',
+            o2:'negro',
+            o3:'azul',
+
+        },
+        almacenamiento: '512GB',
+        camaraFrontal: true,
+        resolucionCamaraFrontal: '100 mpx'
+    },
+    {
+        marca:'samsung',
+        precio:2000000,
+        garantia:false,
+        colores:{
+            o1:'rosado',
+            o2:'amarillo',
+            o3:'azul',
+
+        }
+        
+    }
+     
+];
+
